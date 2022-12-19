663d714acc9d0e24c2620dd8abd6732dc4701786
florianLopitaux <florian.lopitaux@etu.univ-amu.fr>
2022-12-19 14:29:58 +0100

    Advencements of the front-end of the drafting advertising view

--- src/gui/drafting_advertising_view.py
+++ src/gui/drafting_advertising_view.py
@@ -1,18 +1,20 @@
+import os
 import customtkinter
-
-from main import App
 
 
 
 class DraftingAdvertisingView(customtkinter.CTkFrame):
     # CONSTRUCTOR
-    def __init__(self, app: App):
+    def __init__(self, app: customtkinter.CTk):
         super().__init__(master=app)
         self.pack(padx=20, pady=20)
 
         self.__app = app
 
         self.__configure_grid()
+
+        self.__build_label_and_text()
+        self.__build_options()
 
 
 
@@ -33,3 +35,29 @@ class DraftingAdvertisingView(customtkinter.CTkFrame):
                 self.grid_rowconfigure(i, weight=1)
         
         self.grid_rowconfigure(3, weight=1)
+
+
+    def __build_label_and_text(self) -> None:
+        """
+        SUMMARY
+        -------
+        This private method is used to create all labels and text widgets in the view.
+        """
+        excel_file_label = customtkinter.CTkLabel(master=self, text="Excel File :")
+        excel_file_label.grid(row=0, column=0)
+
+        self.__message_text_box = customtkinter.CTkTextbox(master=self, font=("Helvetica", 12))
+        self.__message_text_box.grid(row=1, column=0, columnspan=3)
+    
+
+    def __build_options(self) -> None:
+        """
+        SUMMARY
+        -------
+        This private method is used to generate all option menu and combo box widgets in the view.
+        """
+        self.__excel_file_combo_box = customtkinter.CTkComboBox(master=self, values=[file for file in os.listdir(
+            os.path.join(os.path.realpath(os.path.dirname(__file__)), "..", "..", "assets"))
+            if (".xlsx" in file or ".xls" in file)])
+
+        self.__excel_file_combo_box.grid(row=0, column=1, columnspan=2)
