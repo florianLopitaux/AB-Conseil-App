f3eee391ce5cfa4f4cee997bf1b1e0ec019ca214
florianLopitaux <florian.lopitaux@etu.univ-amu.fr>
2022-12-19 08:42:23 +0100

    Create buttons function to home view

--- src/gui/home_view.py
+++ src/gui/home_view.py
@@ -16,11 +16,11 @@ class HomeView(customtkinter.CTkFrame):
                 The window of the application
         """
         super().__init__(master=app, fg_color="transparent")
-        self.pack(ipadx=200, ipady=70)
+        self.pack(ipadx=210, ipady=80)
+        self.__app = app
 
         # title
-        title_label = customtkinter.CTkLabel(master=self, text="AB Conseil Whatsapp app", wraplength=300,
-                                             font=("Helvetica bold", 16))
+        title_label = customtkinter.CTkLabel(master=self, text="AB Conseil Whatsapp app", font=("Comic Sans MS", 30, "bold"), wraplength=250)
         title_label.pack(pady=(50, 0))
 
         # buttons
@@ -43,11 +43,23 @@ class HomeView(customtkinter.CTkFrame):
         self.__container = customtkinter.CTkFrame(master=self)
         self.__container.pack(pady=(70, 0), ipadx=100, fill="none", expand=False)
 
-        button_use_app = customtkinter.CTkButton(master=self.__container, text="Use app")
+        button_use_app = customtkinter.CTkButton(master=self.__container, command=self.__command_use_app, text="Use app")
         button_use_app.pack(pady=(50, 30))
 
-        button_settings = customtkinter.CTkButton(master=self.__container, text="Settings")
+        button_settings = customtkinter.CTkButton(master=self.__container, command=self.__command_settings, text="Settings")
         button_settings.pack(pady=10)
 
-        button_exit = customtkinter.CTkButton(master=self.__container, text="Exit")
+        button_exit = customtkinter.CTkButton(master=self.__container, command=self.__command_exit, text="Exit")
         button_exit.pack(pady=(10, 50))
+
+
+    def __command_use_app(self) -> None:
+        pass
+
+
+    def __command_settings(self) -> None:
+        pass
+
+
+    def __command_exit(self) -> None:
+        pass
