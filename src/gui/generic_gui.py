a2f2a3e46e6e2bba2a2ee8e35feee31a0c9d4d64
florianLopitaux <florian.lopitaux@etu.univ-amu.fr>
2022-12-19 09:35:39 +0100

    Implements two generic functions to create custom button

--- src/gui/generic_gui.py
+++ src/gui/generic_gui.py
@@ -15,3 +15,43 @@ def clear_widgets(container: customtkinter.CTkFrame) -> None:
     """
     for widget in container.winfo_children():
         widget.destroy()
+
+
+def create_red_button(container: customtkinter.CTkFrame, button_text: str) -> customtkinter.CTkButton:
+    """
+    SUMMARY
+    -------
+    This method genrates and return a custom red button (typically used for a negative button).
+
+    ARGUMENTS
+    ---------
+        container : customtkinter.CTkFrame
+            The frame that contains the button.
+        button_text : str
+            The text that we want inside on the button.
+
+    RETURNS
+    -------
+    customtkinter.CTkButton: the generated button requested.
+    """
+    return customtkinter.CTkButton(master=container, text=button_text, fg_color="#B22222", hover_color="#F08080")
+
+
+def create_green_button(container: customtkinter.CTkFrame, button_text: str) -> customtkinter.CTkButton:
+    """
+    SUMMARY
+    -------
+    This method genrates and return a custom green button (typically used for a positive button).
+
+    ARGUMENTS
+    ---------
+        container : customtkinter.CTkFrame
+            The frame that contains the button.
+        button_text : str
+            The text that we want inside on the button.
+
+    RETURNS
+    -------
+    customtkinter.CTkButton: the generated button requested.
+    """
+    return customtkinter.CTkButton(master=container, text=button_text, fg_color="#32CD32", hover_color="#7CFC00")
