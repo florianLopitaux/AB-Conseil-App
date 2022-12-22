23002dce8bbc7be45c4e6a37985b01bfb5af5617
florianLopitaux <florian.lopitaux@etu.univ-amu.fr>
2022-12-22 10:43:58 +0100

    Finishing DuplicateAnalysisView front-end

--- src/gui/duplicate_analysis_view.py
+++ src/gui/duplicate_analysis_view.py
@@ -1,6 +1,8 @@
+import os
 import customtkinter
 
 from data.settings import Settings
+from gui.generic_gui import clear_frame, create_red_button
 
 
 
@@ -8,12 +10,29 @@ class DuplicateAnalysisView(customtkinter.CTkFrame):
     # CONSTRUCTOR
     def __init__(self, app: customtkinter.CTk) -> None:
         super().__init__(master=app)
-        self.pack(padx=35, pady=35)
 
         self.__app = app
 
+        self.__back_button = create_red_button(self.__app, Settings.get_instance().get_text("DuplicateAnalysisView", "backButton"))
+        self.__back_button.configure(command=self.__command_back)
+        self.__back_button.pack(padx=(0, 230), pady=(5, 0))
+
+        self.pack(padx=35, pady=35)
+        
+        # build widgets' view
+        self.__excel_file_combo_box = customtkinter.CTkComboBox(master=self, values=[file for file in os.listdir(
+            os.path.join(os.path.realpath(os.path.dirname(__file__)), "..", "..", "assets"))
+            if ".xls" in file])
+
+        self.__excel_file_combo_box.set("")
+        self.__excel_file_combo_box.grid(row=0, column=1, sticky="ew", padx=(10, 30), pady=30)
+
         self.__build_labels()
         self.__build_entries()
+
+        analysis_button = customtkinter.CTkButton(master=self, command=self.__command_analysis,
+                                                  text=Settings.get_instance().get_text("DuplicateAnalysisView", "analysisButton"))
+        analysis_button.grid(row=4, column=0, columnspan=2, pady=30)
 
 
 
@@ -24,17 +43,17 @@ class DuplicateAnalysisView(customtkinter.CTkFrame):
         -------
         This private method is called only in the constructor and is used to create all labels of the view.
         """
-        excel_file_label = customtkinter.CTkLabel(master=self, text=Settings.get_instance().get_text("DraftingAdvertisingView", "fileLabel"))
-        excel_file_label.grid(row=0, column=0, pady=30)
+        excel_file_label = customtkinter.CTkLabel(master=self, text=Settings.get_instance().get_text("DuplicateAnalysisView", "fileLabel"))
+        excel_file_label.grid(row=0, column=0, padx=(30, 10), pady=30)
 
-        letter_column_label = customtkinter.CTkLabel(master=self, text=Settings.get_instance().get_text("PopUpParameter", "letterColumnLabel"))
-        letter_column_label.grid(row=1, column=0, columnspan=2, pady=10)
+        letter_column_label = customtkinter.CTkLabel(master=self, text=Settings.get_instance().get_text("DuplicateAnalysisView", "letterColumnLabel"))
+        letter_column_label.grid(row=1, column=0, padx=(30, 10), pady=10)
 
-        start_row_label = customtkinter.CTkLabel(master=self, text=Settings.get_instance().get_text("PopUpParameter", "firstRowLabel"))
-        start_row_label.grid(row=2, column=0, columnspan=2, pady=10)
+        start_row_label = customtkinter.CTkLabel(master=self, text=Settings.get_instance().get_text("DuplicateAnalysisView", "firstRowLabel"))
+        start_row_label.grid(row=2, column=0, padx=(30, 10), pady=10)
 
-        end_row_label = customtkinter.CTkLabel(master=self, text=Settings.get_instance().get_text("PopUpParameter", "lastRowLabel"))
-        end_row_label.grid(row=3, column=0, columnspan=2, pady=10)
+        end_row_label = customtkinter.CTkLabel(master=self, text=Settings.get_instance().get_text("DuplicateAnalysisView", "lastRowLabel"))
+        end_row_label.grid(row=3, column=0, padx=(30, 10), pady=10)
     
 
     def __build_entries(self) -> None:
@@ -44,10 +63,22 @@ class DuplicateAnalysisView(customtkinter.CTkFrame):
         This private method is called only in the constructor and is used to create all entries to get the user input data.
         """
         self.__letter_column_entry = customtkinter.CTkEntry(master=self)
-        self.__letter_column_entry.grid(row=1, column=1, columnspan=2, sticky="ew", pady=10)
+        self.__letter_column_entry.grid(row=1, column=1, padx=(10, 30), pady=10)
 
         self.__start_row_entry = customtkinter.CTkEntry(master=self)
-        self.__start_row_entry.grid(row=2, column=1, columnspan=2, sticky="ew", pady=10)
+        self.__start_row_entry.grid(row=2, column=1, padx=(10, 30), pady=10)
 
         self.__end_row_entry = customtkinter.CTkEntry(master=self)
-        self.__end_row_entry.grid(row=3, column=1, columnspan=2, sticky="ew", pady=10)
+        self.__end_row_entry.grid(row=3, column=1, padx=(10, 30), pady=10)
+
+    
+    
+    # BUTTON FUNCTION
+    def __command_back(self) -> None:
+        self.__back_button.destroy()
+        clear_frame(self)
+        self.__app.change_view('HOME')
+
+
+    def __command_analysis(self) -> None:
+        pass
